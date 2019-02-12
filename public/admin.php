12fb9a2aa393bfb45e56e3fde9fc8a711012669f
gxg <21114046@qq.com>
2019-02-12 10:48:54 +0800

    初步分批上传-3

---
+++ public/admin.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+namespace think;
+
+require __DIR__ . '/../application/mainadmin/common.php';
+// 加载基础文件
+require __DIR__ . '/../thinkphp/base.php';
+
+
+
+// 支持事先使用静态方法设置Request对象和Config对象
+/* 数据目录*/
+define('DATA_PATH', '../Data/');
+
+// 执行应用并响应
+Container::get('app')->run()->send();
