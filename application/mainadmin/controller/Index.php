f1609ed0c7b748fc24939b63b9c6ec28ed1d05ac
gxg <21114046@qq.com>
2019-03-14 17:17:51 +0800

    后端登陆后增加自动执行定期结算

--- application/mainadmin/controller/Index.php
+++ application/mainadmin/controller/Index.php
@@ -1,5 +1,6 @@
 <?php
 namespace app\mainadmin\controller;
+use Think\Db;
 use app\AdminController;
 /**
  * 后台首页
@@ -16,6 +17,27 @@ class Index extends AdminController
 		if(class_exists('app\shop\model\OrderModel')){
 			//执行订单自动签收
 			(new \app\shop\model\OrderModel)->autoSign();			
+		}
+		//执行定期分佣到帐
+		if(class_exists('app\distribution\model\EvalArrivalLogModel')){			
+			$DividendInfo = settings('DividendInfo');
+			if ($DividendInfo['settlement_day'] > 0){
+				$EvalArrivalLogModel = new \app\distribution\model\EvalArrivalLogModel();
+				$log_time = $EvalArrivalLogModel->order('log_id DESC')->value('log_time');//获取最近操作的时间
+				if (time() > $log_time + $settlement_day * 86400){					
+					$inData['log_time'] = time();					
+					Db::startTrans();//事务启用
+					$res = $EvalArrivalLogModel->save($inData);
+					if ($res >= 1){
+						$res = (new \app\distribution\model\DividendModel)->evalArrival(0,$EvalArrivalLogModel->log_id);
+						if ($res == true){
+							Db::commit();//事务提交						
+						}			
+					}					
+					Db::rollback();//事务回滚		
+					
+				}
+			}
 		}
         return $this->fetch('index');
     }
