ae0d030974154f124d3178bf40f828619bfa51d3
zhouxuezhong <1062763849@qq.com>
2020-03-11 12:31:08 +0800

    叮叮市场页面 售出页面

---
+++ application/ddkc/controller/Trade.php
@@ -0,0 +1,40 @@
+<?php
+/*------------------------------------------------------ */
+//-- 会员登陆注册
+//-- Author: iqgmy
+/*------------------------------------------------------ */
+namespace app\ddkc\controller;
+use app\ClientbaseController;
+use app\mining\model\MiningTradeModel;
+use think\Db;
+use think\facade\Cache;
+use think\facade\Session;
+
+use app\ddkc\model\BuyTradeModel;
+use app\ddkc\model\SellTradeModel;
+
+class Trade  extends ClientbaseController{
+    //*------------------------------------------------------ */
+    //-- 初始化
+    /*------------------------------------------------------ */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Model = new BuyTradeModel();
+    }
+    //*------------------------------------------------------ */
+    //-- 叮叮市场
+    /*------------------------------------------------------ */
+    public function index(){
+        $this->assign('title', '叮叮市场');
+        return $this->fetch('index');
+    }
+
+    //*------------------------------------------------------ */
+    //-- 出售DDB
+    /*------------------------------------------------------ */
+    public function sell_ddb(){
+        $this->assign('title', 'DDB挂售');
+        return $this->fetch('sell_ddb');
+    }
+}
