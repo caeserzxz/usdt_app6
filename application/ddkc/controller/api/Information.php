bcc3db7b06b41599d6cc88940c80285d4c660a96
Fariy <13838438888@163.com>
2020-03-12 18:24:48 +0800

    资讯列表&详情页

---
+++ application/ddkc/controller/api/Information.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\ddkc\controller\api;
+use think\Db;
+use app\ApiController;
+use app\member\model\UsersModel;
+use app\member\model\AccountLogModel;
+use app\ddkc\model\DdInformationModel;
+use app\mainadmin\model\SettingsModel;
+/*------------------------------------------------------ */
+//-- 矿机相关逻辑
+/*------------------------------------------------------ */
+
+class Information extends ApiController
+{
+    /*------------------------------------------------------ */
+    //-- 优先执行
+    /*------------------------------------------------------ */
+    public function initialize()
+    {
+        parent::initialize();
+		$this->checkLogin();//验证登陆
+        $this->Model = new DdInformationModel();
+    }
+    /*------------------------------------------------------ */
+	//-- 获取所有矿机列表
+	/*------------------------------------------------------ */
+ 	public function getInformationList(){
+
+		$this->sqlOrder = 'id DESC';
+		$where[] = ['is_show' ,'eq' ,1];
+        $data = $this->getPageList($this->Model,$where);
+        if (count($data['list']) > 0) {
+        	foreach ($data['list'] as $key => $value) {
+        		$data['list'][$key]['add_data'] = date('m-d H:i',$value['add_time']);
+        	}
+        }
+		return $this->ajaxReturn($data);
+	}
+}
