2108a13c40eb65b45a61dbc04f702ac551392c7f
zhouxuezhong <1062763849@qq.com>
2020-03-12 18:58:31 +0800

    挂售逻辑

--- application/ddkc/controller/api/Trade.php
+++ application/ddkc/controller/api/Trade.php
@@ -55,7 +55,7 @@ class Trade extends ApiController
         $stage_id = input('stage_id');
         $stage_info = $TradingStageModel->where('id',$stage_id)->find();
         if(empty($stage_info)){
-            return $this->ajaxReturn(['code' => 0,'msg' => '','url' => '']);
+            return $this->ajaxReturn(['code' => 0,'msg' => '场次不存在','url' => '']);
         }
         if($user['account']['use_integral']<$stage_info['scribe_integral']){
             return $this->ajaxReturn(['code' => 0,'msg' => '信用积分不足,无法预约','url' => '']);
@@ -77,7 +77,7 @@ class Trade extends ApiController
 
         if($res){
             //预约成功后,扣除用户的信用积分
-            $charge['ddb_money']   = -$stage_info['scribe_integral'];
+            $charge['use_integral']   = -$stage_info['scribe_integral'];
             $charge['change_desc'] = '预约扣除信用积分';
             $charge['change_type'] = 11;
             $res1 =$accountModel->change($charge, $user['user_id'], false);
@@ -93,5 +93,85 @@ class Trade extends ApiController
 
     }
 
+    /*------------------------------------------------------ */
+    //-- 挂售下单
+    /*------------------------------------------------------ */
+    public function sell_trade(){
+        $userModel = new UsersModel();
+        $accountModel = new AccountLogModel();
+        $SellTradeMoel = new SellTradeModel();
+        $TradingStageModel = new TradingStageModel();
+        $settints = settings();
+        # 是否登录
+        $user = $userModel->info($this->userInfo['user_id']);
+        if (!$user) {
+            return $this->ajaxReturn(['code' => 0,'msg' => '请先登录','url' => url('passport/login')]);
+        }
+        $number  = input('number');//出售的ddb
+        $stage_id = input('stage_id');
+        #场次信息
+        $stage_info = $TradingStageModel->where('id',$stage_id)->find();
+        if(empty($stage_info)){
+            return $this->ajaxReturn(['code' => 0,'msg' => '场次不存在','url' => '']);
+        }
+        #扣除手续费后的叮叮
+        $service_charge = $number *($settints['service_charge']/100);
+        $dingding = $number *(1-($settints['service_charge']/100));
 
+        $daybegin=strtotime(date("Ymd"));
+        $dayend=$daybegin+86400;
+        #今日售出数量
+        $today_sell_num = $SellTradeMoel->where('sell_start_time','between',[$daybegin,$dayend])->count();
+        $today_sell_total = $SellTradeMoel->where('sell_start_time','between',[$daybegin,$dayend])->sum('sell_number');
+        if($number<$stage_info['trade_min_num']) return $this->ajaxReturn(['code' => 0,'msg' => '该场次最低售出数量为'.$stage_info['trade_min_num'],'url' => '']);
+        if($number>$stage_info['trade_max_num']) return $this->ajaxReturn(['code' => 0,'msg' => '该场次最高售出数量为'.$stage_info['trade_max_num'],'url' => '']);
+        if($number<$settints['min_number']) return $this->ajaxReturn(['code' => 0,'msg' => '叮叮每次最低挂售'.$settints['min_number'],'url' => '']);
+        if($number>$settints['max_number']) return $this->ajaxReturn(['code' => 0,'msg' => '叮叮每次最高挂售'.$settints['max_number'],'url' => '']);
+        if($today_sell_num>=$settints['max_second']) return $this->ajaxReturn(['code' => 0,'msg' => '叮叮每天最多挂售'.$settints['max_second'].'次','url' => '']);
+        if(($today_sell_total+$number)>$settints['max_total_number']) return $this->ajaxReturn(['code' => 0,'msg' => '叮叮每天最多挂售总额为'.$settints['max_total_number'],'url' => '']);
+
+
+        Db::startTrans();
+        $addData = [
+            'sell_user_id'=>$user['user_id'],
+            'sell_stage_id'=>$stage_id,
+            'sell_number'=>$dingding,
+            'sell_status'=>0,
+            'sell_start_time'=>time(),
+            'old_ddb_money'=>$number,
+            'service_charge'=>$settints['service_charge']
+        ];
+        $res = $SellTradeMoel->create($addData);
+        if (!$res) {
+            Db::rollback();
+            return $this->ajaxReturn(['code' => 0,'msg' => '挂售失败','url' => '']);
+        }
+        #挂售成功后
+        if($res){
+            #除手续费
+            $charge['ddb_money']   = -$service_charge;
+            $charge['change_desc'] = '扣除挂售手续费';
+            $charge['change_type'] = 12;
+            $res1 =$accountModel->change($charge, $user['user_id'], false);
+            if(!$res1){
+                Db::rollback();
+                return $this->ajaxReturn(['code' => 0,'msg' => '扣除挂售手续费失败','url' => '']);
+            }
+            #扣除DDB
+            $charge['ddb_money']   = -$dingding;
+            $charge['change_desc'] = '扣除挂售的DDB';
+            $charge['change_type'] = 12;
+            $res1 =$accountModel->change($charge, $user['user_id'], false);
+            if(!$res1){
+                Db::rollback();
+                return $this->ajaxReturn(['code' => 0,'msg' => '扣除挂售的DDB失败','url' => '']);
+            }
+
+        }
+
+        # 预约成功
+        Db::commit();
+        return $this->ajaxReturn(['code' => 1,'msg' => '挂售成功','url' => url('trade/index')]);
+
+    }
 }
