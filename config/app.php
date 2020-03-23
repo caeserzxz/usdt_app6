16f9b885d9ffe2163fce84c45b84446fb774111d
zhouxuezhong <1062763849@qq.com>
2020-03-23 09:24:23 +0800

    修改默认模块

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     // +----------------------------------------------------------------------
 
     // 默认模块名
-    'default_module'         => defined('INDEX')?'ddkc':'trade',
+    'default_module'         => defined('INDEX')?'ddkc':'shop',
     // 禁止访问模块
     'deny_module_list'       => defined('INDEX')?['common','mainadmin']:['common'],
     // 默认控制器名
