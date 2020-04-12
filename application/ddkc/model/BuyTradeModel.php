0d82ea639d12122ab8027cd7b49ae754d12ab090
zhouxuezhong <1062763849@qq.com>
2020-04-12 20:17:37 +0800

    返还信用积分

--- application/ddkc/model/BuyTradeModel.php
+++ application/ddkc/model/BuyTradeModel.php
@@ -272,7 +272,7 @@ class BuyTradeModel extends BaseModel
                 $buy_info = [];
                 $buy_info = $this->where('id',$v)->find();
                 $charge = [];
-                $charge['use_integral']   = $buy_info['scribe_integral'];
+                $charge['use_integral']   = $buy_info['deduct_integral'];
                 $charge['change_desc'] = '开奖结束,返还信用积分';
                 $charge['change_type'] = 11;
                 $accountModel->change($charge, $buy_info['buy_user_id'], false);
